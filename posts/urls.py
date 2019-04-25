87448c64bf41514ae6eb94eb2cf54a5fae95d478
kyeah01 <kyeah01@gmail.com>
2019-04-25 11:42:29 +0000

    04 | update & delete

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name='posts'
 
 urlpatterns = [
+    path('<int:post_pk>/delete/', views.delete, name='delete'),
+    path('<int:post_pk>/update/', views.update, name='update'),
     path('create/', views.create, name='create'),
-    path('<int:post_pk>', views.detail, name='detail'),
+    path('<int:post_pk>/', views.detail, name='detail'),
     path('', views.list, name='list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 from .models import Post
 from .forms import PostForm
 
@@ -21,11 +22,30 @@ def create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save()
-            return redirect('posts:list')
-            # return redirect('posts:detail', post.pk)
+            # return redirect('posts:list')
+            return redirect('posts:detail', post.pk)
     else:
         form = PostForm()
     context = {
         'form' : form,
     }
-    return render(request, 'posts/create.html', context)
+    return render(request, 'posts/postform.html', context)
+    
+def update(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:detail', post.pk)
+    else:
+        context = {
+            'form' : PostForm(instance=post)
+        }
+    return render(request, 'posts/postform.html', context)
+
+@require_POST
+def delete(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    post.delete()
+    return redirect('posts:list')
