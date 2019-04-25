2b55e2a076899897cc9a47f1a4be498a4cee296c
kyeah01 <kyeah01@gmail.com>
2019-04-25 11:08:47 +0000

    02 | forms & list

--- lucky/settings.py
+++ lucky/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lucky.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'lucky', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def list(request):
-    pass
+    posts = Post.objects.all()
+    context = {
+        'posts' : posts,
+    }
+    return render(request, 'posts/list.html', context)
+    
