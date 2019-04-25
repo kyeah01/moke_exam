4f7b7fcdc5a12bc6005dd325baa01725ca81190b
kyeah01 <kyeah01@gmail.com>
2019-04-25 11:32:50 +0000

    03 | detail

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name='posts'
+
 urlpatterns = [
+    path('create/', views.create, name='create'),
+    path('<int:post_pk>', views.detail, name='detail'),
     path('', views.list, name='list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
@@ -10,3 +10,22 @@ def list(request):
     }
     return render(request, 'posts/list.html', context)
     
+def detail(request, post_pk):
+    context = {
+        'post' : get_object_or_404(Post, pk=post_pk),
+    }
+    return render(request, 'posts/detail.html', context)
+    
+def create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:list')
+            # return redirect('posts:detail', post.pk)
+    else:
+        form = PostForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'posts/create.html', context)
