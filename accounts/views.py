95234f9c4ac5ff1810043b598f7a8b03484973f9
kyeah01 <kyeah01@gmail.com>
2019-04-26 00:42:23 +0000

    10 | test

--- accounts/views.py
+++ accounts/views.py
@@ -1,41 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.views.decorators.http import require_POST
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
 
 # Create your views here.
 def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            auth_login(request, user)
-            return redirect('posts:list')
-    else:
-        form = UserCreationForm()
-    context = {
-        'users' : get_user_model().objects.count(),
-        'form' : form
-    }
+    # 2.
+    
     return render(request, 'accounts/auth_form.html', context)
     
 def login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            auth_login(request, user)
-            return redirect('posts:list')
-    else:
-        form = AuthenticationForm()
-    context = {
-        'form' : form,
-    }
+    #1.
+    
     return render(request, 'accounts/auth_form.html', context)
     
-@login_required
 def logout(request):
-    auth_logout(request)
+    # 3.
     return redirect('posts:list')
