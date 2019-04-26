3dd6d3a8b52eb1e046150e97bd89b8a2adfcb5a6
kyeah01 <kyeah01@gmail.com>
2019-04-26 00:38:00 +0000

    09 | real_final

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def signup(request):
@@ -15,6 +16,7 @@ def signup(request):
     else:
         form = UserCreationForm()
     context = {
+        'users' : get_user_model().objects.count(),
         'form' : form
     }
     return render(request, 'accounts/auth_form.html', context)
